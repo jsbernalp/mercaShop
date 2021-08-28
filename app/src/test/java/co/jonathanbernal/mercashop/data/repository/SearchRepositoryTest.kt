91b99179edd1d9f74ec794572a12688e8da0302e
Jonathan Bernal <jsbernalp@gmail.com>
2021-08-27 21:21:26 -0500

    the error in the tests is solved

--- app/src/test/java/co/jonathanbernal/mercashop/data/repository/SearchRepositoryTest.kt
+++ app/src/test/java/co/jonathanbernal/mercashop/data/repository/SearchRepositoryTest.kt
@@ -1,9 +1,7 @@
 package co.jonathanbernal.mercashop.data.repository
 
 import co.jonathanbernal.mercashop.data.remote.api.SearchApi
-import co.jonathanbernal.mercashop.domain.models.Paging
-import co.jonathanbernal.mercashop.domain.models.Product
-import co.jonathanbernal.mercashop.domain.models.SearchResponse
+import co.jonathanbernal.mercashop.domain.models.*
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
@@ -32,7 +30,9 @@ class SearchRepositoryTest {
     @Test
     fun `searchProductsInApi, llama al metodo searchProductsInApi, obtiene un objeto de SearchResponse, mapea el objeto y retorna un listado de productos `() {
         val paging = Paging(123456,0,10)
-        val product = Product("MLA717131873","Silla Eames Comedor Base Madera Varios Colores - Pack X 4",13765.5,500,250,"http://http2.mlstatic.com/D_804309-MLA46605828763_072021-O.jpg")
+        val eshop = Eshop("asdasda")
+        val seller = Seller(eshop)
+        val product = Product("MLA717131873","Silla Eames Comedor Base Madera Varios Colores - Pack X 4",13765.5,seller,500,250,"http://http2.mlstatic.com/D_804309-MLA46605828763_072021-O.jpg")
         val results = arrayListOf(product, product)
         val searchResponse = SearchResponse("MLA",paging,results)
 
